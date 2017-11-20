92e17c02917b4b530cb2a230d32313b3b3f77c18
mostafa69d <mostafa69d@gmail.com>
2017-11-20 16:45:24 -0500

    fix: resolve add decks bug

--- actions/decks.js
+++ actions/decks.js
@@ -2,12 +2,12 @@ import { GET_ALL_DECKS, ADD_NEW_DECK, ADD_DECKS } from './types'
 import { getAllDecksFromStorage, addADeckToStorage, addDecksToStorage } from '../utils/helpers'
 
 
-export function getAllDecks({ decks }) {
-    return {
-        type: GET_ALL_DECKS,
-        decks
-    }
-}
+// export function getAllDecks({ decks }) {
+//     return {
+//         type: GET_ALL_DECKS,
+//         decks
+//     }
+// }
 
 export function addNewDeck({ name }) {
     return {
@@ -23,13 +23,15 @@ export function addDecks({ decks }) {
     }
 }
 
-export function fetchAllDecks() {
-    return function(dispatch) {
-        return getAllDecksFromStorage().then((decks) => {
-            dispatch(getAllDecks({ decks }))
-        })
-    }
-}
+// export function fetchAllDecks() {
+//     return function(dispatch) {
+//         return getAllDecksFromStorage().then((decks) => {
+//             console.log("from actions")
+//             console.log(decks)
+//             dispatch(getAllDecks({ decks }))
+//         })
+//     }
+// }
 
 export function addDecksAsync({ decks }) {
     return function(dispatch) {
